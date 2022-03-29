e6654e2c9e2e089f2dafb1b09d579bb735ee682f
chrisYang256 <maxsummer256@gmail.com>
2022-03-29 20:53:13 +0900

    create apis

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from dotenv import load_dotenv
 
 import os
 import pymysql
+import datetime
 
 app = Flask(__name__)
-load_dotenv(".env.dev")
+
+load_dotenv(
+    dotenv_path=".env",
+    verbose=True
+)
 
 db = pymysql.connect(
 	user    = os.getenv("DB_USER"),
@@ -23,73 +28,142 @@ def index():
 
 @app.route("/company/api/list", methods=["GET"])
 def compamy_list():
-    # offset = request.args.get('start', default=0, type=int)
-    # limit  = request.args.get('limit', default=20, type=int)
-    sql_values = "select * from company"
-    cursor.execute(sql_values)
+    db.connect()
+
+    offset = request.args.get('offset', default=1, type=int)
+    limit  = request.args.get('limit', default=20, type=int)
+    skip   = int(limit * (offset - 1))
+
+    compaies = """
+        select * 
+        from company 
+        order by cdate DESC 
+        limit %s offset %s
+    """
+
+    cursor.execute(compaies, (limit, skip))
 
     results = cursor.fetchall() # all/one 구분하기
 
+    if results is None:
+        db.close()
+        return { "message": "아직 입력된 기업이 존재하지 않습니다.", "status": 200 }
+
+    db.close()
     return { "results": results, "status": 200 }
 
 @app.route("/company/api/get", methods=["GET"]) # query param 방식
-def compamy_get():
+def get_compamy():
+    db.connect()
+
     company_name = request.args.get("company_name", default="", type=str)
 
     if company_name:
-        sql_value = "select * from company where company_name=%s"
-        cursor.execute(sql_value, (company_name))
+        company = """
+            select * 
+            from company 
+            where company_name=%s
+        """
+        cursor.execute(company, (company_name))
 
         result = cursor.fetchone()
 
+        if result is None:
+            db.close()
+            return { "message": "존재하지 않는 기업입니다.", "status": 200 }
+
+        db.close()
         return { "result": result, "status": 200 }
-    
-    else:
-        return { "message": "회사 이름이 입력되지 않았습니다.", "status": 200 }
+
+    db.close()
+    return { "message": "기업 이름이 입력되지 않았습니다.", "status": 200 }
 
 @app.route("/company/api/add", methods=["POST"])
-def compant_add():
+def add_compant():
     db.connect()
+
     req = request.get_json()
     company_name = req["company_name"]
     description  = req["description"]
 
-    sql_value = "insert into company(company_name, description) values(%s, %s)"
+    company_info = """
+        insert into company(company_name, description) 
+        values(%s, %s)
+    """
+    cursor.execute(company_info, (company_name, description))
 
-    cursor.execute(sql_value, (company_name, description))
     db.commit()
     db.close()
-
     return { "message": "success", "Status": 201 }
 
 
 @app.route("/company/api/update/<company_id>", methods=["PUT"]) # path param 방식
-def company_update(company_id: int):
+def update_company(company_id: int):
     db.connect()
     req = request.get_json()
 
     company_name = req["company_name"]
     description  = req["description"]
+    now_date     = datetime.datetime.now()
 
-    sql_value = "update company set company_name=%s, description=%s where id=%s"
+    find_company = """
+        select * 
+        from company
+        where id=%s
+    """
+    cursor.execute(find_company, (company_id))
+    find_company = cursor.fetchone()
 
-    cursor.execute(sql_value, (company_name, description, company_id))
+    if find_company is None:
+        db.close()
+        return { "message:": "존재하지 않는 기업입니다.", "status": 200 }
+
+    company_info = """
+        update company 
+        set company_name=%s, description=%s, udate=%s
+        where id=%s
+    """
+
+    cursor.execute(company_info, 
+        (company_name, description, now_date, company_id)
+    )
+    cursor.fetchall()
+
     db.commit()
     db.close()
-
     return { "message": "success", "Status": 201 }
 
 @app.route("/company/api/remove/<company_id>", methods=["DELETE"])
-def company_remove(company_id: int):
+def remove_company(company_id: int):
     db.connect()
 
-    sql_value = "delete from company where id=%s"
+    find_company = """
+        select * 
+        from company
+        where id=%s
+    """
+    cursor.execute(find_company, (company_id))
+    find_company = cursor.fetchone()
 
-    cursor.execute(sql_value, (company_id))
+    if find_company is None:
+        db.close()
+        return { "message:": "존재하지 않는 기업입니다.", "status": 200 }
+
+    company_info = """
+        delete from company 
+        where id=%s
+    """
+
+    cursor.execute(company_info, (company_id))
+    cursor.fetchall()
+
     db.commit()
     db.close()
-
     return { "message": "success", "Status": 201 }
-    
+
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(
+        host  = os.getenv("WEB_HOST"), 
+        port  = os.getenv("WEB_PORT"), 
+        debug = os.getenv("DEBUG")
+    )
