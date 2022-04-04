7e16a18ab579cab7591ecc26c1ca9768fab93e79
chrisYang256 <maxsummer256@gmail.com>
2022-04-04 16:35:27 +0900

    add: user list api/view

--- routes/company_view.py
+++ routes/company_view.py
@@ -6,16 +6,16 @@ import threading
 
 # def server_time():
 #     threading.Timer(1, server_time).start()
-#     return print('{}'.format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+#     return '{}'.format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
 @company_view.route("list_view")
 def get_company_list():
     # timer = server_time()
-    return render_template("index.html")
+    return render_template("select_company_list.html")
 
 @company_view.route("detail_view/<company_id>")
 def get_company_detail(company_id):
-    return render_template("company_detail.html", company_id=company_id)
+    return render_template("select_company_detail.html", company_id=company_id)
 
 @company_view.route("add_view")
 def add_company():
